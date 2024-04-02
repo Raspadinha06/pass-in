f3903338d5d3196c955a18117efd3bf3a011032a
Davi Marinho <davimarinho7742@gmail.com>
2024-04-02 10:18:26 -0300

    refactor: moved to attendee package

--- src/main/java/rocketseat/com/passin/domain/attendee/Attendee.java
+++ src/main/java/rocketseat/com/passin/domain/attendee/Attendee.java
@@ -1,10 +1,11 @@
-package rocketseat.com.passin.domain;
+package rocketseat.com.passin.domain.attendee;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import rocketseat.com.passin.domain.event.Event;
 
 import java.time.LocalDateTime;
 
@@ -29,6 +30,6 @@ public class Attendee {
     @JoinColumn(name = "event_id", nullable = false)
     private Event event;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false)
     private LocalDateTime createdAt;
 }
