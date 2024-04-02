87b122018701ed6435e422e8b95781643e8c675b
Davi Marinho <davimarinho7742@gmail.com>
2024-04-02 10:21:55 -0300

    feat: create CheckinRepository.java

---
+++ src/main/java/rocketseat/com/passin/repositories/CheckinRepository.java
@@ -0,0 +1,6 @@
+package rocketseat.com.passin.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import rocketseat.com.passin.domain.checkin.Checkin;
+
+public interface CheckinRepository extends JpaRepository<Checkin, Long> {}
