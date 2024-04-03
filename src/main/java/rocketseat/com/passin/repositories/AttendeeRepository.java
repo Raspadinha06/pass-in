b320b4250323785c879e73f5733febb4e8ede140
Davi Marinho <davimarinho7742@gmail.com>
2024-04-03 08:09:11 -0300

    fix: Long to String

--- src/main/java/rocketseat/com/passin/repositories/AttendeeRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/AttendeeRepository.java
@@ -3,4 +3,9 @@ package rocketseat.com.passin.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.attendee.Attendee;
 
-public interface AttendeeRepository extends JpaRepository<Attendee, Long> {}
+import java.util.List;
+
+public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    public List<Attendee> findByEventId(String eventId);
+
+}
--- src/main/java/rocketseat/com/passin/repositories/EventRepository.java
+++ src/main/java/rocketseat/com/passin/repositories/EventRepository.java
@@ -3,4 +3,4 @@ package rocketseat.com.passin.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.event.Event;
 
-public interface EventRepository extends JpaRepository<Event, Long> {}
+public interface EventRepository extends JpaRepository<Event, String> {}
