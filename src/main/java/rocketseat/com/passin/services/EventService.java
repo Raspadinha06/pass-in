c384f1d36dab18014c9c970e2caa04ee6368a7bd
Davi Marinho <davimarinho7742@gmail.com>
2024-04-03 13:52:39 -0300

    refactor: repository to service

--- src/main/java/rocketseat/com/passin/services/EventService.java
+++ src/main/java/rocketseat/com/passin/services/EventService.java
@@ -4,10 +4,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import rocketseat.com.passin.domain.attendee.Attendee;
 import rocketseat.com.passin.domain.event.Event;
+import rocketseat.com.passin.domain.event.exceptions.EventNotFoundException;
 import rocketseat.com.passin.dtos.events.EventIdDTO;
 import rocketseat.com.passin.dtos.events.EventRequestDTO;
 import rocketseat.com.passin.dtos.events.EventResponseDTO;
-import rocketseat.com.passin.repositories.AttendeeRepository;
 import rocketseat.com.passin.repositories.EventRepository;
 
 import java.text.Normalizer;
@@ -17,13 +17,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class EventService {
     private final EventRepository eventRepository;
-    private final AttendeeRepository attendeeRepository;
+    private final AttendeeService attendeeService;
 
     public EventResponseDTO getEventDetail(String eventId){
         Event event = this.eventRepository.findById(eventId)
-                .orElseThrow(() -> new RuntimeException("Could not find event with ID: "
+                .orElseThrow(() -> new EventNotFoundException("Could not find event with ID: "
                 + eventId));
-        List<Attendee> attendeeList = this.attendeeRepository.findByEventId(eventId);
+        List<Attendee> attendeeList = this.attendeeService.getAllAttendeesFromEvent(eventId);
         return new EventResponseDTO(event, attendeeList.size());
     }
 
