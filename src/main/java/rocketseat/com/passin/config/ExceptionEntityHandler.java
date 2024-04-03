419cdb4058a49057c7a768e58f91aaba1f8af169
Davi Marinho <davimarinho7742@gmail.com>
2024-04-03 13:50:24 -0300

    feat: create ExceptionEntityHandler.java

---
+++ src/main/java/rocketseat/com/passin/config/ExceptionEntityHandler.java
@@ -0,0 +1,14 @@
+package rocketseat.com.passin.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import rocketseat.com.passin.domain.event.exceptions.EventNotFoundException;
+
+@RestControllerAdvice
+public class ExceptionEntityHandler {
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity handleEventNotFound(EventNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+}
