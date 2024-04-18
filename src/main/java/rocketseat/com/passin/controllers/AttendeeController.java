54b7ccc1b63eeb2ee016c5e2783e7b15f824d53f
Davi Marinho <davimarinho7742@gmail.com>
2024-04-18 08:53:39 -0300

    feat: adding SpringDoc

--- src/main/java/rocketseat/com/passin/controllers/AttendeeController.java
+++ src/main/java/rocketseat/com/passin/controllers/AttendeeController.java
@@ -1,5 +1,9 @@
 package rocketseat.com.passin.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,15 +15,28 @@ import rocketseat.com.passin.services.CheckInService;
 @RestController
 @RequestMapping("/attendees")
 @RequiredArgsConstructor
+@Tag(name = "pass-in")
 public class AttendeeController{
     private final AttendeeService attendeeService;
 
+    @Operation(summary = "Displays the badge of an attendee.", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Attendee badge found successfully!"),
+            @ApiResponse(responseCode = "404", description = "Badge not found."),
+            @ApiResponse(responseCode = "500", description = "Data search error."),
+    })
     @GetMapping("{attendeeId}/badge")
     public ResponseEntity<AttendeeBadgeResponseDTO> getAttendeeBadge(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder){
         AttendeeBadgeResponseDTO response = this.attendeeService.getAttendeeBadge(attendeeId, uriComponentsBuilder);
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Check into an event.", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Checked in successfully!"),
+            @ApiResponse(responseCode = "400", description = "Invalid parameters."),
+            @ApiResponse(responseCode = "409", description = "Already checked in!"),
+    })
     @PostMapping("/{attendeeId}/check-in")
     public ResponseEntity<Object> registerCheckIn(@PathVariable String attendeeId, UriComponentsBuilder uriComponentsBuilder){
         this.attendeeService.checkInAttendee(attendeeId);
